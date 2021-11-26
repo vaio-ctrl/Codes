83b2aa566de4aff6f3f4a91df990fbd5a15edc40
vaio-ctrl <85823236+vaio-ctrl@users.noreply.github.com>
2021-11-26 20:31:41 +0530

    Fix for Uppercase letters

--- translation.py
+++ translation.py
@@ -1,9 +1,12 @@
 def translate(phrase):
     translation = ""
     for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "x"
+        if letter.lower() in "aeiou":
+            if letter.isuppBer():
+                translation = translation + "X"
+            else:
+                translation = translation + "x"
         else:
             translation = translation + letter
     return translation
-print(translate(input("Enter the phrase: ")))
+print(translate(input("Phrase: ")))
